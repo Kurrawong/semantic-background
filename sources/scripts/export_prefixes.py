bd0dd38dcd3b0448d119ca91affc0d960111a6dc
Edmond Chuc <edmond@kurrawong.ai>
2024-07-16 11:48:50 +1000

    feat: improve publish-prefixes.yml and export_prefixes.py
    - use poetry to execute python scripts
    - improve directory structure
    - only generate prefixes where the prefix has a value
    - deletes the files in the output folder before generation

--- sources/scripts/export_prefixes.py
+++ sources/scripts/export_prefixes.py
@@ -1,16 +1,23 @@
 import json
 from pathlib import Path
-from rdflib import Graph, Namespace, Literal, URIRef
+from rdflib import Graph
 
 parent_dir = Path(__file__).parent.parent.parent
+output_dir = parent_dir / "output/gh-pages/ns"
+output_dir.mkdir(parents=True, exist_ok=True)
+
+for item in output_dir.glob('**/*'):
+    if item.is_file:
+        item.unlink()
 
 g = Graph()
 for f in Path(parent_dir / "ontologies").glob("*.ttl"):
     g.parse(f)
 
-
 for ns_prefix, namespace in g.namespaces():
-    print(ns_prefix, namespace)
+    # Empty prefixes are valid, only run with prefixes that have a value.
+    if ns_prefix:
+        print(ns_prefix, namespace)
 
-    with open(f'ns/{ns_prefix}.file.json', 'w') as f:
-        json.dump({ns_prefix: namespace}, f)
+        with open(output_dir / f"{ns_prefix}.file.json", "w") as f:
+            json.dump({ns_prefix: namespace}, f)
