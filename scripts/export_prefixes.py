834104601bb1a29d28c97b7a8434c7cb5f619c2b
Jae <26292620+JaeTLDR@users.noreply.github.com>
2025-10-07 21:52:08 +1000

    Update export_prefixes.py

--- scripts/export_prefixes.py
+++ scripts/export_prefixes.py
@@ -1,9 +1,10 @@
 import json
 from pathlib import Path
 from rdflib import Graph
+import re
 
 parent_dir = Path(__file__).parent.parent.parent
-output_dir = parent_dir / "sources/gh-pages/ns"
+output_dir = parent_dir / "gh-pages/ns"
 output_dir.mkdir(parents=True, exist_ok=True)
 
 for item in output_dir.glob('**/*'):
@@ -14,6 +15,9 @@ g = Graph()
 for f in Path(parent_dir / "originals").glob("*.ttl"):
     g.parse(f)
 
+protocolRegex = re.compile(r"^(https:\/\/|http:\/\/)", re.IGNORECASE)
+specialRegex = re.compile(r"\W", re.IGNORECASE)
+
 for ns_prefix, namespace in g.namespaces():
     # Empty prefixes are valid, only run with prefixes that have a value.
     if ns_prefix:
@@ -21,3 +25,7 @@ for ns_prefix, namespace in g.namespaces():
 
         with open(output_dir / f"{ns_prefix}.file.json", "w") as f:
             json.dump({ns_prefix: namespace}, f)
+        
+        safe_file_name = specialRegex.sub("-", protocolRegex.sub("",namespace))
+        with open(output_dir / f"{safe_file_name}.file.json", "w") as f:
+            json.dump({namespace: ns_prefix}, f)
